9f80bea931cd4c4356f69014d6a8ed07ed79ba12
Yann <yannmadoire@hotmail.fr>
2023-03-24 11:38:11 +0100

    air02end

--- brouillon2.js
+++ brouillon2.js
@@ -1,48 +1,22 @@
 const args = process.argv.slice(2)
 
-arg1 = args[0]
-arg2 = args[1]
+let arg2 = (args[args.length-1]);
+args.pop()
+let arg1 = args
 
+let tab = ['coucou', 'cest', 'moi']
+let sep = ['-']
 
-function letArrayArg(argument, array){ //  ---> fonction qui met les arguments sous forme de tableaux à raison de 1 mot = 1 élément
-
-   
-    let element = ""
-    let séparateur = " "
-
-
-    for(let i = 0 ; i <= argument.length ; i++){
-        if((argument[i] != séparateur) && (i != argument.length)){
-        element = `${element}${argument[i]}`
-        
-        } 
-        else if((argument[i]==séparateur) || (i == argument.length)){
-        array.push(element)
-        element = ""
-        }
-    }return array
-
-}
-
-function split_en_fonction(string_à_couper, string_séparateur){
+function concat(array_de_strings, séparateur){
     
-    let array_string_à_couper = []
-    let array_string_séparateur = []
-    letArrayArg(string_à_couper, array_string_à_couper)
-    letArrayArg(string_séparateur, array_string_séparateur) 
-
-    let tableau = array_string_à_couper[0]    
+    let string = ""
     
-    for (let i = 1 ; i < array_string_à_couper.length ; i++){
-        if(array_string_à_couper[i] != array_string_séparateur[0]){
-        tableau = `${tableau} ${array_string_à_couper[i]}`
+    for (let i = 0 ; i < array_de_strings.length ; i++){        
+        if(i == array_de_strings.length-1){
+            string = `${string}${array_de_strings[i]}`
         }
-        else if (array_string_à_couper[i] == array_string_séparateur[0]){
-        array_string_à_couper.splice(i, 1)
-        tableau = `${tableau}\n${array_string_à_couper[i]}`    
-        }
-    } return tableau
+        else string = `${string}${array_de_strings[i]}${séparateur}`    
+    }return string
 }
-
-console.log(split_en_fonction(arg1, arg2));
+console.log(concat(tab, sep));
 
